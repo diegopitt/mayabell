3b71833244316b32e144f41619abb1e67829382b
Diego Makana <diego.pittaluga@gmail.com>
2020-02-17 14:25:47 -0800

    ups 879

--- layout/Tours.js
+++ layout/Tours.js
@@ -78,7 +78,7 @@ class Tours extends Component {
       <Grid container spacing={0} className={classes.gridMapColor}>
         <Grid item xs={12} md={6}>
           <Paper className={classes.paperRuins} elevation={4}>
-            <Typography component="p" variant="subtitle" color='secondary' className={classes.eRuins}>
+            <Typography component="p" variant="subtitle1" color='secondary' className={classes.eRuins}>
             Explora Las Ruinas
             </Typography>
             <Link href="/tours">
@@ -91,7 +91,7 @@ class Tours extends Component {
         </Grid>
         <Grid item xs={12} md={6}>
           <Paper className={classes.paperWaterfall} elevation={4}>
-            <Typography component="p" variant="subtitle" color='secondary' className={classes.eRuins}>
+            <Typography component="p" variant="subtitle1" color='secondary' className={classes.eRuins}>
               Explora Agua Azul
             </Typography>
             <Link href="/tours">
